Fixed precision issues

models/GWT.py
@@ -32,10 +32,13 @@ class GraphWaveletTransform():
         return F1, u
 
     def second_order_feature(self,u):
-        u1 = torch.empty((self.ro.shape)).to(self.device)
+        u1 = torch.zeros((self.ro.shape)).to(self.device)
         for j in range(len(self.psi)):
             for j_prime in range(0,j):
-                u1 = torch.cat((u1, torch.abs(self.psi[j_prime]@u[j])), 1)
+                if(j_prime==0 and j==0):
+                    u1 = torch.abs(self.psi[j_prime]@u[j])
+                else:
+                    u1 = torch.cat((u1, torch.abs(self.psi[j_prime]@u[j])), 1)
             # u1 = torch.cat((u1, torch.matrix_power(self.adj,16)@u[j]),1)
         return u1
 

models/graph_learning.py
@@ -95,7 +95,8 @@ class GraphFeatLearningLayer(nn.Module):
 
     def forward(self, point_cloud, eps):
         
-        W = torch.cdist(point_cloud.to(self.device)*torch.sqrt(self.alphas), point_cloud.to(self.device)*torch.sqrt(self.alphas)) 
+        W = torch.cdist(point_cloud.to(self.device)*torch.sqrt(self.alphas), point_cloud.to(self.device)*torch.sqrt(self.alphas), compute_mode='donot_use_mm_for_euclid_dist')
+        # import pdb; pdb.set_trace() 
         W = 1/(W + eps)
         W[W==(1/eps)] = 0
         W[W<self.threshold] = 0